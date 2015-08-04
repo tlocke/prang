a0b6da3e650b0d0465522b66e1a0927d94f5e6ca
Tony Locke <tlocke@tlocke.org.uk>
2015-08-04 19:19:05 +0100

    Installed versioneer

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 
 from setuptools import setup
+import versioneer
 
 setup(
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     name="prang",
     description="A pure Python RELAX NG validator.",
     author="Tony Locke",
